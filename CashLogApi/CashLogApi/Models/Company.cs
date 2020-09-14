7f8b435793326ce21b5a0e7a71afdd36460dfec3
thiagofranzener <thiagofranzener@hotmail.com>
2020-09-13 22:25:58 -0300

    Reescrito a parte do Utils
    Reescrito a parte de métodos utilitários que serão utilizados em todo o projeto.

--- CashLogApi/CashLogApi/Models/Company.cs
+++ CashLogApi/CashLogApi/Models/Company.cs
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace CashLogApi.Models
 {
-    class Company
+    class Company : IEquatable<Company>
     {
+        
+        int _id, _number;
+        string _name, _cnpj, _adress, _complement, _city, _state, _cep;
+        Guid _token;
+
+        public int Id { get => _id; set => _id = value; }
+        public int Number { get => _number; set => _number = value; }
+        public string Name { get => _name; set => _name = value; }
+        public string Cnpj { get => _cnpj; set => _cnpj = value; }
+        public string Adress { get => _adress; set => _adress = value; }
+        public string Complement { get => _complement; set => _complement = value; }
+        public string City { get => _city; set => _city = value; }
+        public string State { get => _state; set => _state = value; }
+        public string Cep { get => _cep; set => _cep = value; }
+        public Guid Token { get => _token; set => _token = value; }
+
+        bool IEquatable<Company>.Equals(Company other)
+        {
+            return (other != null && other.Id == this.Id);
+        }
     }
 }
--- CashLogApi/LazyDevUtils/Helpers.cs
+++ CashLogApi/LazyDevUtils/Helpers.cs
@@ -1,12 +1,66 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace LazyDevUtils
+namespace CashLogUtils
 {
     class Helpers
     {
+
+        public double ValNumber(string value)
+        {
+            double result;
+            try
+            {
+                result = Double.Parse(value);
+            } catch 
+            {
+                result = 0;
+            }
+            return result;
+        }
+
+        public bool IsNumber(string value)
+        {
+            try
+            {
+                Double.Parse(value);
+            }
+            catch 
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public bool ObjectSerializationIsInvalid(Object value, List<string> errors)
+        {
+            if (value == null)
+            {
+                errors.Add("Erro ao serializar objeto");
+                return true;
+            }
+            return false;
+        }
+
+        public bool IsDateValid(DateTime data)
+        {
+            if (data == null) return false;
+            if (data < new DateTime(1900, 1, 1)) return false;
+            if (data > new DateTime(2400, 1, 1)) return false;
+            return true;
+        }
+
+        public string EncryptString(string value)
+        {
+            string result = "";
+            System.Security.Cryptography.MD5CryptoServiceProvider cryptoService = new System.Security.Cryptography.MD5CryptoServiceProvider();
+            byte[] bytesToHash = System.Text.Encoding.ASCII.GetBytes(value);
+            foreach (byte streamByte in bytesToHash)
+            {
+                result += streamByte.ToString("N2");
+            }
+            return result.ToUpper();
+        }
+
     }
 }
