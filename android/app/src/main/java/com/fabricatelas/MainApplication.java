088de2f5697fd73f14597c2e53a87329e8b78d20
Jonathan Witkosky <42014543+jonathanwitkosky@users.noreply.github.com>
2020-12-07 01:58:02 -0300

    Atualização de nome

--- android/app/src/main/java/com/fabricatelas/MainApplication.java
+++ android/app/src/main/java/com/fabricatelas/MainApplication.java
@@ -1,4 +1,4 @@
-package com.fabricatelas;
+package com.Cashlog;
 
 import android.app.Application;
 import android.content.Context;
@@ -62,7 +62,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.fabricatelas.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.Cashlog.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
--- ios/fabricaTelasTests/fabricaTelasTests.m
+++ ios/fabricaTelasTests/fabricaTelasTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface fabricaTelasTests : XCTestCase
+@interface CashlogTests : XCTestCase
 
 @end
 
-@implementation fabricaTelasTests
+@implementation CashlogTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
